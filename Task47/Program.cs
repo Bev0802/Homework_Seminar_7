262fd54a9baedfa7b507dd92d737f3c4f3cb74dd
Katy <bev08021982@gmail.com>
2022-11-20 12:11:42 +0400

    Homework_Seminar_7

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -3,4 +3,41 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-Console.WriteLine("Hello, World!");
+
+//МЕТОД
+//метода создает двухмерный массив заполненный вещественными числами.
+double[,] CreateMatrixRndDouble(int rows, int columns,  double min, double max)
+{
+    double[,] matrix = new double[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double num = rnd.NextDouble() * (max - min) + min; // создаем число массива в заданном диапазоне
+            matrix[i, j] =  Math.Round(num, 1);
+        }
+    }
+    return matrix;
+}
+
+//метод печтатает двухмерный массив.
+void PrintMatrixDouble(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} | ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+//ВЫВОД РЕЗУЛЬТАТА
+double[,] array2D = CreateMatrixRndDouble(3, 4, -10, 10);
+PrintMatrixDouble(array2D);
+
--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,7 +1,73 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // 1, 7 -> такого элемента в массиве нет
-Console.WriteLine("Hello, World!");
+
+//МЕТОДЫ
+//метода создает массив из произвольных чисел.
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+//метод печатает массив.
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+//метод ищет позицию.
+
+int SearchElement(int[,] matrix, int row, int column)
+{
+    row--;
+    column--;
+    int element = matrix[row, column];
+    return element;
+}
+
+//ПРОГРАММА
+
+int[,] array2D = CreateMatrixRndInt(5, 5, 1, 10);
+PrintMatrix(array2D);
+
+
+Console.Write("Введите строку позиции, которую хотие вывести: ");
+int numRow = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите колонку позиции, которую хотие вывести: ");
+int numColumn = Convert.ToInt32(Console.ReadLine());
+
+//проверка ввода.
+if (numRow > array2D.GetLength(0) || numColumn > array2D.GetLength(1) || numRow <= 0 || numColumn<=0)
+{
+    Console.WriteLine("Такого элемента в массиве нет");
+    return;
+}
+
+//Вывод результата.
+int elementmatrix = SearchElement(array2D, numRow, numColumn);
+Console.WriteLine(elementmatrix);
